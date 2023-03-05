b5c420801061264dcf572a25a5fe19ca784052a9
antonina <antrapsh@gmail.com>
2023-03-05 14:25:42 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
-import './App.css';import MainPage from "./pages/main-page/main-page";import Header from "./components/header/header";function App() {    return (        <div className="App">            <Header/>            <MainPage/>        </div>    );}export default App;
+import './App.css';
+import MainPage from "./pages/main-page/main-page";
+import Header from "./components/header/header";
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <MainPage/>
+        </div>
+    );
+}
+
+export default App;
