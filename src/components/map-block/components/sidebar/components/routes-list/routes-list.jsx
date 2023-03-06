aab54ff480fc3466f330c28fb0dfb7cfa3e78f7c
Antonina Trapsh <82774971+AntoninaTrapsh@users.noreply.github.com>
2023-03-06 13:15:48 +0300

    added dnd logic (#4)

--- src/components/map-block/components/sidebar/components/routes-list/routes-list.jsx
+++ src/components/map-block/components/sidebar/components/routes-list/routes-list.jsx
@@ -1,37 +1,10 @@
 import styled from "styled-components";
 import React from "react";
-
-const Button = styled.button`
-    background-color: transparent;
-    color: #8d91a0;
-    border: none;
-    cursor: pointer;
-    
-    &:hover {
-        color: black;
-    }
-`;
+import RouteElement from "../route-element/route-element";
 
 const RoutesListContainer = styled.div`
     display: flex,
     justify-content: center;
-`
-
-const Route = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    padding: 3px 0;
-    margin: 0;
-    border-bottom: 1px solid #bdbdbd;
-    cursor: pointer;
-    
-    &:hover {
-        background-color: rgb(235 239 239);
-        border-bottom: 1px solid #03a9f4;
-    }
 `
 
 const Title = styled.h3`
@@ -43,7 +16,6 @@ const EmptyWarning = styled.p`
 `
 
 const RoutesList = ({routes, handleDelete}) => {
-
     return (
         <>
             <Title>Выбранный маршрут</Title>
@@ -51,12 +23,9 @@ const RoutesList = ({routes, handleDelete}) => {
                 routes.length ?
                 <RoutesListContainer>
                     {
-                        routes.map((route) => {
+                        routes.map((route, index) => {
                             return (
-                                <Route key={route.id}>
-                                    <p>{route.address}</p>
-                                    <Button onClick={() => handleDelete(route.id)}>&#10006;</Button>
-                                </Route>
+                                <RouteElement key={route.id} route={route} handleDelete={handleDelete} index={index}/>
                             )
                         })
                     }
