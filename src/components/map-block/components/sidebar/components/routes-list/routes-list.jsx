38f753278828ff88ee0a3c07b8cf42b9b7dd3492
antonina <antrapsh@gmail.com>
2023-03-08 12:18:40 +0300

    added some styles fix

--- src/components/map-block/components/sidebar/components/routes-list/routes-list.jsx
+++ src/components/map-block/components/sidebar/components/routes-list/routes-list.jsx
@@ -3,18 +3,16 @@ import React from "react";
 import RouteElement from "../route-element/route-element";
 import Loader from "../../../../../loader/loader";
 import {WARNING_TEXT} from "../../../../../../utils/const";
+import SearchWarning from "../search-warning/search-warning";
 
 const RoutesListContainer = styled.div`
     display: flex;
     justify-content: center;
+    flex-direction: column;
 `
 
 const Title = styled.h3`
     margin-bottom: 7px;
-`
-
-const EmptyWarning = styled.p`
-    text-align: left;
 `
 
 const RoutesList = ({routes, handleDelete, isLoading}) => {
@@ -35,7 +33,7 @@ const RoutesList = ({routes, handleDelete, isLoading}) => {
                                         })
                                     }
                                 </RoutesListContainer> :
-                                <EmptyWarning>{WARNING_TEXT.NO_POINTS}</EmptyWarning>
+                                <SearchWarning>{WARNING_TEXT.NO_POINTS}</SearchWarning>
                         )
             }
         </>
